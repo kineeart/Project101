95e83c6c81552ab09ddc848c959eb0acbcf6e1ba
NguyenMinhKien04 <nguyenminhkien5577@gmail.comm>
2026-07-10 14:19:23 +0700

    fix defaultpipeline

--- src/main/java/com/example/middleware/feature/orchestration/application/builder/DefaultPipelineContextBuilder.java
+++ src/main/java/com/example/middleware/feature/orchestration/application/builder/DefaultPipelineContextBuilder.java
@@ -1,6 +1,9 @@
 package com.example.middleware.feature.orchestration.application.builder;
-
+import com.example.middleware.feature.orchestration.application.factory.ExecutionFactory;
+import com.example.middleware.feature.metadata.application.builder.MappingContextBuilder;
+import com.example.middleware.feature.metadata.application.builder.MappingContextBuilder;
 import com.example.middleware.feature.orchestration.application.PipelineContext;
+import com.example.middleware.feature.orchestration.application.factory.ExecutionFactory;
 import com.example.middleware.feature.orchestration.domain.Execution;
 import com.example.middleware.feature.processing.domain.context.MappingContext;
 import com.example.middleware.feature.processing.domain.event.RawEvent;
@@ -9,7 +12,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class DefaultPipelineContextBuilder
         implements PipelineContextBuilder {
+private final ExecutionFactory executionFactory;
 
+private final MappingContextBuilder mappingContextBuilder;
+public DefaultPipelineContextBuilder(
+        ExecutionFactory executionFactory,
+        MappingContextBuilder mappingContextBuilder) {
+
+    this.executionFactory = executionFactory;
+    this.mappingContextBuilder = mappingContextBuilder;
+}
     @Override
     public PipelineContext build(
             RawEvent event,
@@ -20,8 +32,17 @@ public class DefaultPipelineContextBuilder
                 new PipelineContext();
 
         context.setRawEvent(event);
-        context.setMappingContext(mappingContext);
-        context.setExecution(execution);
+        context.setMappingContext(
+        mappingContextBuilder.build(
+                event.getProfileId()
+        )
+);
+
+context.setExecution(
+        executionFactory.create(
+                event.getEventId()
+        )
+);
 
         return context;
     }
