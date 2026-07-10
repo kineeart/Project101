44b4d75ce74fd59dec84c8a9bf10d8e19b125766
NguyenMinhKien04 <nguyenminhkien5577@gmail.comm>
2026-07-10 09:18:26 +0700

    fix defaultmappingcontextbuilder

--- src/main/java/com/example/middleware/feature/metadata/application/builder/DefaultMappingContextBuilder.java
+++ src/main/java/com/example/middleware/feature/metadata/application/builder/DefaultMappingContextBuilder.java
@@ -1,15 +1,20 @@
 package com.example.middleware.feature.metadata.application.builder;
 
+import com.example.middleware.feature.metadata.application.MetadataService;
 import com.example.middleware.feature.processing.domain.context.MappingContext;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DefaultMappingContextBuilder
         implements MappingContextBuilder {
-
+private final MetadataService metadataService;
+public DefaultMappingContextBuilder(
+        MetadataService metadataService) {
+    this.metadataService = metadataService;
+}
     @Override
     public MappingContext build(String profileId) {
-
+metadataService.toString();
         return new MappingContext();
 
     }
