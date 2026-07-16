c7c77fb9d07b6e1f4bcbd4e3c9ced0e01d50edcf
NguyenMinhKien04 <nguyenminhkien5577@gmail.comm>
2026-07-16 14:39:35 +0700

    second test, pass

--- src/test/java/com/example/middleware/feature/delivery/builder/CsvFileBuilderTest.java
+++ src/test/java/com/example/middleware/feature/delivery/builder/CsvFileBuilderTest.java
@@ -73,5 +73,49 @@ class CsvFileBuilderTest {
                 outputFile.lines().get(2)
         );
     }
+    @Test
+void shouldBuildWithoutTrailerWhenTrailerDisabled() {
 
+    CsvFormatter formatter = new CsvFormatter();
+    CsvFileBuilder builder = new CsvFileBuilder(formatter);
+
+    TransformedEvent event = new TransformedEvent();
+
+    Map<String, Object> payload = new LinkedHashMap<>();
+
+    payload.put("ITEM", "1001");
+    payload.put("PRICE", 25000);
+    payload.put("SHOP", "HCM01");
+
+    event.setPayload(payload);
+
+    DeliveryProfile profile =
+            new DeliveryProfile(
+                    "D:/ignore",
+                    "ORACLE_",
+                    "csv",
+                    ",",
+                    true,
+                    false,   // <-- tắt trailer
+                    "TRAILER||RECORD_COUNT|{count}"
+            );
+
+    OutputFile outputFile =
+            builder.build(event, profile);
+
+    assertEquals(
+            2,
+            outputFile.lines().size()
+    );
+
+    assertEquals(
+            "ITEM,PRICE,SHOP",
+            outputFile.lines().get(0)
+    );
+
+    assertEquals(
+            "1001,25000,HCM01",
+            outputFile.lines().get(1)
+    );
+}
 }
